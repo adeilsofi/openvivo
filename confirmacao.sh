26def319e60ed66ee2a1024669b6c5f82bd926b4
adeilsofi <adeilsofi@yahoo.com>
2020-04-20 19:51:53 -0400

    Create confirmacao.sh

---
+++ confirmacao.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+clear
+echo -e "\E[44;1;37m    PAINEL-SSH/DROP/SOCKS/SSL INSTALADO     \E[0m"
+echo ""
+echo -e "                          \033[1;31mBy @adeilsonfi\033[1;36m"
+echo -e "   VIP-vps" | figlet
+echo -e "   Painel-SSH" | figlet
+echo -e "   V.25" | figlet
+echo ""
