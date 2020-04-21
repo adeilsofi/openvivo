a3c88a6c50d5aabee34021bc15d6c7e9dc4adf8d
adeilsofi <adeilsofi@yahoo.com>
2020-04-20 20:14:08 -0400

    Update confirmacao.sh

--- confirmacao.sh
+++ confirmacao.sh
@@ -3,8 +3,8 @@
 IP=$(wget -qO- ipv4.icanhazip.com)
 clear
 echo -e "   Painel-SSH" | figlet
-echo -e "   V.25" | figlet
-echo -e "                          \033[1;31mBy @adeilsonfi\033[1;36m"
+echo -e "      V.25" | figlet
+echo -e "                 \033[1;31mBy @adeilsonfi\033[1;36m"
 echo ""
 echo -e "\033[1;36m SEU PAINEL:\033[1;37m http://$IP/\033[0m"
 echo -e "\033[1;36m USUARIO:\033[1;37m admin\033[0m"
